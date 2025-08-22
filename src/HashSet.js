564aaf0d7a5d9452ce85502e3dc38c4e6dab19c3
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-08-22 18:17:50 -0400

    Created hash set class

--- src/HashSet.js
+++ src/HashSet.js
@@ -1,8 +1,8 @@
-import LinkedList from "hashmap-linked-list";
+import LinkedList from "hashset-linked-list";
 
 
 const linkedList = new LinkedList().appendNode()
-class HashMap {
+class HashSet {
     constructor() {
         this.buckets = new Array(16).fill(null);
         this.loadFactor = 0.75;
@@ -20,30 +20,30 @@ class HashMap {
         return hashCode;
     }
 
-    setKey(key, value) {
+    setKey(key) {
         const bucketIndex = this.hash(key);
-        // If the keys bucket index  is empty, create and insert linked list and add node with bucketIndex, key and value and update size
+        // If the keys bucket index  is empty, create and insert linked list and add node with bucketIndex, key and update size
         if(!this.buckets[bucketIndex]) {     
             this.buckets[bucketIndex] = new LinkedList();
-            this.buckets[bucketIndex].prependNode(bucketIndex,key,value);
+            this.buckets[bucketIndex].prependNode(bucketIndex,key);
             this.size++;
-        } else { // If there is a collision, check for duplicate keys and and add node with bucketIndex, key and value and update size
+        } else { // If there is a collision, check for duplicate keys and and add node with bucketIndex, key and update size
             if (this.buckets[bucketIndex].listContainsKey(key)) {
                 const duplicateKeyIndex = this.buckets[bucketIndex].findListKeyIndex(key);
                 this.buckets[bucketIndex].removeNodeAt(duplicateKeyIndex);
-                this.buckets[bucketIndex].prependNode(bucketIndex,key,value);   
+                this.buckets[bucketIndex].prependNode(bucketIndex,key);   
             } else {
-                this.buckets[bucketIndex].prependNode(bucketIndex,key,value);
+                this.buckets[bucketIndex].prependNode(bucketIndex,key);
                 this.size++;
             }; 
         };  
-        // If the size of hashmap is greater than threshold, double the capacity and rehash
+        // If the size of hashset is greater than threshold, double the capacity and rehash
         if (this.size > (this.loadFactor * this.capacity)) {
             this.capacity = this.capacity * 2;
-            const hashMapEntries = this.returnHashMapEntries();
-            this.clearHashMap();
-            for (let i = 0; i < hashMapEntries.length; i++) {
-               this.setKey(hashMapEntries[i][0],hashMapEntries[i][1]);
+            const hashSetEntries = this.returnHashSetEntries();
+            this.clearHashSet();
+            for (let i = 0; i < hashSetEntries.length; i++) {
+               this.setKey(hashSetEntries[i][0],hashSetEntries[i][1]);
             };
         };
 
@@ -55,7 +55,7 @@ class HashMap {
         if(this.buckets[bucketIndex]) {
             const keyIndex = this.buckets[bucketIndex].findListKeyIndex(key)
             if(!this.buckets[bucketIndex].findListKeyIndex(key)) {
-                return this.buckets[bucketIndex].getNodeAtIndex(keyIndex).value;
+                return this.buckets[bucketIndex].getNodeAtIndex(keyIndex).key;
             } else {
                return null;
             }
@@ -77,7 +77,7 @@ class HashMap {
             return false;
         };      
      
-    }
+    };
 
     removeKey(key) {
         const bucketIndex = this.hash(key);
@@ -97,54 +97,41 @@ class HashMap {
         }
     };
 
-    lengthOfHashMap() {
+    lengthOfHashSet() {
         return this.size;
     };
 
-    clearHashMap() {
+    clearHashSet() {
         this.buckets = new Array(this.capacity).fill(null);
         this.size = 0;
     };
 
    returnKeysArray() {
-        let hashMapKeys = [];
+        let hashSetKeys = [];
         for (let i = 0; i < this.buckets.length; i++) {
             if (this.buckets[i]) {
                 let currentNode = this.buckets[i].head
                 while (currentNode) {
-                    hashMapKeys.push(currentNode.key)
+                    hashSetKeys.push(currentNode.key)
                     currentNode = currentNode.nextNode;
                 };
             }
         }
-        return hashMapKeys;
+        return hashSetKeys;
     };
 
-    returnValuesArray() {
-        const hashMapKeys = [];
-        for (let i = 0; i < this.buckets.length; i++) {
-            if (this.buckets[i]) {
-                let currentNode = this.buckets[i].head;
-                while (currentNode) {
-                    hashMapKeys.push(currentNode.value);
-                    currentNode = currentNode.nextNode;
-                };
-            };
-        }
-        return hashMapKeys;
-    };
+    
 
-    returnHashMapEntries() {
-        const hashMapEntries = [];
-        const hashMapKeys = this.returnKeysArray();
-        const hashMapValues = this.returnValuesArray();
-        // Add each key value pair to hashMapEntries array
-        for (let i = 0;i < hashMapKeys.length; i++) {
-            hashMapEntries[i] = [hashMapKeys[i], hashMapValues[i]]
+    returnHashSetEntries() {
+        const hashSetEntries = [];
+        const hashSetKeys = this.returnKeysArray();
+        // Add each key to hashSetEntries array
+        for (let i = 0;i < hashSetKeys.length; i++) {
+            hashSetEntries[i] = hashSetKeys[i]
         }
-        return hashMapEntries;
+        return hashSetEntries;
 
     }
 };
 
-export default HashMap;
+export default HashSet;
